7f0696e6d5fda257b551688999d0beccb06a3913
evrahimhasan <evrahimhasanemran.com>
2026-01-08 16:41:39 +0600

    solved problem1

---
+++ Problem1.js
@@ -0,0 +1,14 @@
+function reverseString(string) {
+    let reversed = "";
+
+    for (let i = string.length - 1; i >= 0; i--) {
+        reversed += string[i];
+    }
+
+    return reversed;
+}
+
+console.log(reverseString('hello'));
+console.log(reverseString('bangladesh'));
+console.log(reverseString('hi'));
+console.log(reverseString('emran'));
